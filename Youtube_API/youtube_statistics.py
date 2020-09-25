c1436d75c04865aed54fd15031102422ebc17477
razielar <razielar@gmail.com>
2020-09-25 22:44:34 +0200

    dump json file

--- Youtube_API/youtube_statistics.py
+++ Youtube_API/youtube_statistics.py
@@ -1,3 +1,5 @@
+import requests
+import json
 
 class YTstats():
 
@@ -8,5 +10,29 @@ class YTstats():
 
     def get_channel_statistics(self):
         url= f'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={self.channel_id}&key={self.api_key}'
-        print(url)
+        #print(url)
+        json_url= requests.get(url)
+        data= json.loads(json_url.text)
+        #print(data)
+
+        try:
+            data= data["items"][0]["statistics"]
+        except: 
+            data= None 
+
+        self.channel_statistics= data
+        
+        return data
+
+    def dump(self):
+        if self.channel_statistics is None:
+            return
+        channel_title= "Vanessa Vega" #get channel name from data
+        channel_title= channel_title.replace(" ", "_").lower()
+        file_name= channel_title + '.json'
+        with open(file_name, 'w') as f:
+            json.dump(self.channel_statistics, f, indent=4)
+        
+        print('File dumped')
+
 
