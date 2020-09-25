40aa40e1f351928dd1bdac6b52f2904338d5b321
razielar <razielar@gmail.com>
2020-09-25 22:16:02 +0200

    method get_channel_statistics outputs json

---
+++ Youtube_API/youtube_statistics.py
@@ -0,0 +1,12 @@
+
+class YTstats():
+
+    def __init__(self, api_key, channel_id):
+        self.api_key= api_key
+        self.channel_id= channel_id
+        self.channel_statistics= None
+
+    def get_channel_statistics(self):
+        url= f'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={self.channel_id}&key={self.api_key}'
+        print(url)
+
